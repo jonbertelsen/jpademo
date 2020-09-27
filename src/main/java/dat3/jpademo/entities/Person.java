014aaddde3296f0d62a6e70217b1de8a2108794a
Jon Bertelsen <jobe@cphbusiness.dk>
2020-09-28 01:19:03 +0200

    First commit

---
+++ src/main/java/dat3/jpademo/entities/Person.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dat3.jpademo.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author jobe
+ */
+@Entity
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long p_id;
+   
+    private String name;
+    private int year;
+    
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Address address;
+
+    @OneToMany(mappedBy = "person",  cascade = CascadeType.PERSIST)
+    private List<Fee> fees;
+    
+    @ManyToMany(mappedBy = "persons",  cascade = CascadeType.PERSIST)
+    private List<SwimStyle> styles;
+
+    public List<SwimStyle> getStyles() {
+        return styles;
+    }
+    
+    public List<Fee> getFees() {
+        return fees;
+    }
+
+    public void AddFee(Fee fee) {
+        this.fees.add(fee);
+        if (fee != null){
+            fee.setPerson(this);
+        }
+    }
+    
+    public void AddSwimStyle(SwimStyle style){
+        if (style != null){
+            this.styles.add(style);
+            style.getPersons().add(this);
+        }
+    }
+    
+    public void removeSwimStyle(SwimStyle swimStyle){
+        if (swimStyle != null){
+            styles.remove(swimStyle);
+            swimStyle.getPersons().remove(this);    
+        }
+    
+    }
+   
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+        if(address != null){
+            address.setPerson(this);
+            
+        }
+    }
+    
+    public Person() {
+    }
+
+    public Long getP_id() {
+        return p_id;
+    }
+
+    public Person(String name, int year) {
+        this.name = name;
+        this.year = year;
+        this.fees = new ArrayList<>();
+        this.styles = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
+}
---
+++ src/main/java/dat3/jpademo/entities/Tester.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dat3.jpademo.entities;
+
+import dto.PersonStyleDTO;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author jobe
+ */
+public class Tester {
+    
+    public static void main(String[] args) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        EntityManager em = emf.createEntityManager();
+        
+        Person p1 = new Person("Jønke", 1963);
+        Person p2 = new Person("Blondie", 1959);
+        
+        Address a1 = new Address("Store Torv 1", 2323, "Nr. Snede");
+        Address a2 = new Address("Langgade 34", 1212, "Valby");
+        
+        p1.setAddress(a1);
+        p2.setAddress(a2);
+        
+        Fee f1 = new Fee(100);
+        Fee f2 = new Fee(200);
+        Fee f3 = new Fee(300);
+       
+        p1.AddFee(f1);
+        p1.AddFee(f3);
+        p2.AddFee(f2);
+        
+        SwimStyle s1 = new SwimStyle("Crawl");
+        SwimStyle s2 = new SwimStyle("ButterFly");
+        SwimStyle s3 = new SwimStyle("Breast stroke");
+        
+        p1.AddSwimStyle(s1);
+        p1.AddSwimStyle(s3);
+        p2.AddSwimStyle(s2);
+                        
+        em.getTransaction().begin();
+            em.persist(p1);
+            em.persist(p2);
+        em.getTransaction().commit();
+        
+        
+        em.getTransaction().begin();
+       //     p1.removeSwimStyle(s3);
+        em.getTransaction().commit();
+        
+       
+        System.out.println("p1: " + p1.getP_id()+ ", " + p1.getName());
+        System.out.println("p2: " + p2.getP_id() + ", " + p2.getName());
+        
+        
+        System.out.println("Jønkes gade: " + p1.getAddress().getStreet());
+        
+        System.out.println("Lad os se om to-vejs virker: " + a1.getPerson().getName());
+        
+        System.out.println("Hvem har betalt f2? Det har: " + f2.getPerson().getName());
+        
+        System.out.println("Hvad er der blevet betalt i alt?");
+    
+        TypedQuery<Fee> q1 = em.createQuery("SELECT f FROM Fee f", Fee.class);
+        List<Fee> fees = q1.getResultList();
+        
+        for (Fee f: fees){
+            System.out.println(f.getPerson().getName() + ": " + f.getAmount() + 
+                    " kr. Den: "+ f.getPayDate() + " Adr: " + f.getPerson().getAddress().getCity());
+        }
+        
+        TypedQuery<Person> q2 = em.createQuery("SELECT p FROM Person p", Person.class);
+        List<Person> persons = q2.getResultList();
+        
+        for (Person p: persons){
+            System.out.println("Navn: " + p.getName());
+            System.out.println("--Fees:");
+            for (Fee f: p.getFees()){
+                System.out.println("---- Beløb: " + f.getAmount() + ", " + f.getPayDate().toString() );
+            }
+            System.out.println("--Styles:");
+            for (SwimStyle ss: p.getStyles()){
+                System.out.println("---- Style: " + ss.getStyleName());
+            }
+        }
+        
+        System.out.println("**** Eksperimenter med JPQL");
+        
+        TypedQuery<PersonStyleDTO> q4 = em.createQuery("SELECT new dto.PersonStyleDTO(p.name, p.year, s.styleName) FROM Person p JOIN p.styles s", dto.PersonStyleDTO.class);
+        List<PersonStyleDTO> personList =  q4.getResultList();
+        
+        for (PersonStyleDTO p: personList){
+            System.out.println(p.getName() + ", " + p.getYear() + ", " + p.getSwimStyle());
+        }
+        
+        
+        
+        
+        
+       
+    }
+    
+}
